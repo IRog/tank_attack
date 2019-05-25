fbee0e0107ab1f8459e435ad5b34af447c35b1f8
Ivy Rogatko <ipr210lehigh@gmail.com>
2019-05-25 16:43:50 -0700

    working follow camera

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -1,5 +1,5 @@
 use crate::tank_attack::{Tank, TankCamera};
-use amethyst::core::{Transform};
+use amethyst::core::{nalgebra::Vector3, Transform};
 use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
 use amethyst::input::InputHandler;
 
@@ -19,17 +19,19 @@ impl<'s> System<'s> for MovementSystem {
     fn run(&mut self, (mut transforms, tank, camera, input): Self::SystemData) {
         let rotation = input.axis_value("rotate_sideways").unwrap();
         let movement = input.axis_value("move_tank").unwrap();
+        let mut tank_translation = Vector3::new(0.0, 0.0, 0.0);
 
         for (_, transform) in (&tank, &mut transforms).join() {
             transform.yaw_global(rotation as f32 * ROTATION_SCALAR);
             transform.move_backward(movement as f32 * MOVEMENT_SCALAR);
+            tank_translation = *transform.translation();
         }
 
         for (_, transform) in (&camera, &mut transforms).join() {
-            if rotation != 0.0 {
-                transform.yaw_local(rotation as f32 * ROTATION_SCALAR * 0.5);
-                transform.move_right(rotation as f32 * 0.25);
-            }
+            transform.yaw_global(rotation as f32 * ROTATION_SCALAR);
+            transform.set_x(tank_translation.x as f32);
+            transform.set_z(tank_translation.z as f32);
+            transform.move_backward(5.0);
             transform.move_forward(movement as f32 * MOVEMENT_SCALAR);
         }
     }
